879b26b1de4c72133669e4780f63f9810c71c243
Modvision <MoD@MEs-MacBook-Pro.local>
2023-09-22 06:08:06 -0500

    made markdown template

---
+++ untils/generateModDown.js
@@ -0,0 +1,38 @@
+function modReadme (data) {
+return `
+# ${data.title}
+
+
+## Discription
+${data.discripton}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#test)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing 
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+Link to email at ${data.email} and github profile [here](${data.github}).
+ `;
+
+}
+
+module.export = modReadme;

653429e3cacb433ead23a753d37e12d9df309ded
Modvision <MoD@MEs-MacBook-Pro.local>
2023-09-22 13:18:37 -0500

    fix bugs and typos

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateREADME = require('./utils/generateModDown');
+const generateMarkdown = require('./utils/generateModDown'); 
+
 
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of you project?',
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Enter the project description:',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Chosse a license for you project:',
-        choices: ['None', 'MIT']
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'How would you like others to contribute',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Enter test instruction',
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter you Github username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter email address',
-    },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a project description:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'None'], 
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
 ];
 
+// Function to write README file
 function writeToFile(fileName, data) {
-fs.writeFile(fileName,data, (err) => {
+  fs.writeFile(fileName, data, (err) => {
     if (err) {
-        console.log(err);
+      console.log(err);
     } else {
-        console.log('readme had been created');
+      console.log('README.md successfully created!');
     }
-    });
-
+  });
 }
+
+// Function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        const modDown = generateREADME(answers);
-        writeToFile('ModMe.md',modDown);
-    });
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers); // Corrected function name
+    writeToFile('README.md', markdownContent);
+  });
 }
 
+// Initialize the application
 init();
--- utils/generateModDown.js
+++ utils/generateModDown.js
@@ -1,17 +1,77 @@
-function markDownData (data) {
-return `
-# ${data.title}
+function renderLicenseBadge(license) {
+  const licenses = {
+    MIT: {
+      badge:
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      link: "[MIT License](https://opensource.org/licenses/MIT)",
+    },
+    "Apache 2.0": {
+      badge:
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      link: "[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)",
+    },
+    "GNU GPLv3": {
+      badge:
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+      link: "[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)",
+    },
+  };
 
+  const selectedLicense = licenses[license];
+  if (!selectedLicense) {
+    return "";
+  }
+  return selectedLicense.badge;
+}
 
-## Discription
-${data.discripton}
+function renderLicenseLink(license) {
+  const licenses = {
+    MIT: {
+      badge:
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      link: "[MIT License](https://opensource.org/licenses/MIT)",
+    },
+    "Apache 2.0": {
+      badge:
+        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      link: "[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)",
+    },
+    "GNU GPLv3": {
+      badge:
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+      link: "[GNU GPLv3 License](https://www.gnu.org/licenses/gpl-3.0)",
+    },
+  };
+  const selectedLicense = licenses[license];
+  if (!selectedLicense) {
+    return "";
+  }
+  return selectedLicense.link;
+}
+
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+
+  let licenseSection = "";
+  if (licenseBadge && licenseLink) {
+    licenseSection = `## License\nThis project is licensed under the ${licenseLink}.`;
+  } else {
+    licenseSection = "## License\nThis project has no specific license.";
+  }
+
+  return `# ${data.title}
+${licenseBadge}
+
+## Description
+${data.description}
 
 ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
-- [License](#license)
+${licenseBadge ? "- [License](#license)" : ""}
 - [Contributing](#contributing)
-- [Test](#test)
+- [Tests](#tests)
 - [Questions](#questions)
 
 ## Installation
@@ -20,18 +80,18 @@ ${data.installation}
 ## Usage
 ${data.usage}
 
-## License
-This project is licensed under the ${data.license} license.
+${licenseSection}
 
-## Contributing 
+## Contributing
 ${data.contributing}
 
 ## Tests
 ${data.tests}
 
 ## Questions
-Link to email at ${data.email} and github profile [here](${data.github}).
- `;
+GitHub: [${data.github}](https://github.com/${data.github})
+Email: ${data.email}
+`;
 }
 
-module.exports = markDownData;
+module.exports = generateMarkdown;
